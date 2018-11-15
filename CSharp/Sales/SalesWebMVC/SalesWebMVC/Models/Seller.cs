b6ab80597328615a87e6997f157e31f2f2b7ea4d
Rafael Coutinho <rafacout@gmail.com>
2018-11-15 17:21:08 -0200

    Outras entidades

---
+++ CSharp/Sales/SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double BaseSalary { get; set; }
+
+        public Department Department { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime date, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Date = date;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(a => a.Date >= initial && a.Date <= final).Sum(a => a.Amount);
+        }
+    }
+}

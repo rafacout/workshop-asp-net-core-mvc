efa64d7a4a9745a8b586b7f6df27b787f0d5e723
Rafael Coutinho <rafacout@gmail.com>
2018-11-16 13:25:08 -0200

    Adicionado a foreignkey

--- CSharp/Sales/SalesWebMVC/SalesWebMVC/Models/Seller.cs
+++ CSharp/Sales/SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -19,6 +19,8 @@ namespace SalesWebMVC.Models
 
         public Department Department { get; set; }
 
+        public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()

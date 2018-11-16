2654f3ca24ad1deb7b73fc04f5f8e2ccfcd4c4de
Rafael Coutinho <rafacout@gmail.com>
2018-11-16 14:13:50 -0200

    Adicionado o nome do departamento na tela de detalhe

--- CSharp/Sales/SalesWebMVC/SalesWebMVC/Services/SellerService.cs
+++ CSharp/Sales/SalesWebMVC/SalesWebMVC/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services
 {
@@ -28,7 +29,7 @@ namespace SalesWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(a => a.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(a => a.Id == id);
         }
 
         public void Remove(int id)

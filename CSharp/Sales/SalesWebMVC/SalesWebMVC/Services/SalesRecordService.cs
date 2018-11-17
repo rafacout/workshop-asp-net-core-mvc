c96510198489afd0cc4c63b83253e68343ba886a
Rafael Coutinho <rafacout@gmail.com>
2018-11-17 15:08:24 -0200

    Implementado a busca simples

---
+++ CSharp/Sales/SalesWebMVC/SalesWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SalesRecordService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj 
+                         in _context.SalesRecord
+                         select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(a => a.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(a => a.Date <= maxDate.Value);
+            }
+
+            return await result.Include(a => a.Seller)
+                .Include(b => b.Seller.Department)
+                .OrderByDescending(c => c.Date)
+                .ToListAsync();
+        }
+    }
+}

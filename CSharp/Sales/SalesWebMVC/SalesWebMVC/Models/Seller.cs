cd6e20295e946e6add1c5ba276d453e12b34b70e
Rafael Coutinho <rafacout@gmail.com>
2018-11-17 11:41:22 -0200

    Adicionadas as formatacoes do modelo

--- CSharp/Sales/SalesWebMVC/SalesWebMVC/Models/Seller.cs
+++ CSharp/Sales/SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +12,16 @@ namespace SalesWebMVC.Models
 
         public string Name { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Display(Name="Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
